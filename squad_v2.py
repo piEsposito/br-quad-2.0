2667f31b2d3f0f4107e67da649078c6f70cb69a3
Pi Esposito <piero.skywalker@gmail.com>
2020-11-12 23:11:33 -0300

    first commit

---
+++ squad_v2.py
@@ -0,0 +1,124 @@
+from __future__ import absolute_import, division, print_function
+
+import json
+import os
+
+import datasets
+
+
+# TODO(squad_v2): BibTeX citation
+_CITATION = """\
+@article{2020braquad,
+       author = {{Esposito}, Wladimir and {Esposito}, Piero and {Tamais},
+                 Ana Laura and {Gatti}, Daniel},
+        title = "{BrQuAD - Brazilian
+                  Question-Answering Dataset: Cria¸c~ao de
+                  dataset para benchmark de modelos de
+                  Machine Learning para question-answering em
+                  Portugu^es brasileiro}",
+         year = 2020,
+}
+"""
+
+_DESCRIPTION = """\
+combines the 100,000 questions in SQuAD1.1 with over 50,000 unanswerable questions written adversarially by crowdworkers
+ to look similar to answerable ones. To do well on SQuAD2.0, systems must not only answer questions when possible, but
+ also determine when no answer is supported by the paragraph and abstain from answering.
+"""
+
+_URL = "https://rajpurkar.github.io/SQuAD-explorer/dataset/"
+_DEV_FILE = "dev-v2.0.json"
+_TRAINING_FILE = "train-v2.0.json"
+
+
+class SquadV2Config(datasets.BuilderConfig):
+    """BuilderConfig for SQUAD."""
+
+    def __init__(self, **kwargs):
+        """BuilderConfig for SQUADV2.
+
+        Args:
+          **kwargs: keyword arguments forwarded to super.
+        """
+        super(SquadV2Config, self).__init__(**kwargs)
+
+
+class SquadV2(datasets.GeneratorBasedBuilder):
+    """TODO(squad_v2): Short description of my dataset."""
+
+    # TODO(squad_v2): Set up version.
+    BUILDER_CONFIGS = [
+        SquadV2Config(name="squad_v2", version=datasets.Version("2.0.0"), description="SQuAD plaint text version 2"),
+    ]
+
+    def _info(self):
+        # TODO(squad_v2): Specifies the datasets.DatasetInfo object
+        return datasets.DatasetInfo(
+            # This is the description that will appear on the datasets page.
+            description=_DESCRIPTION,
+            # datasets.features.FeatureConnectors
+            features=datasets.Features(
+                {
+                    "id": datasets.Value("string"),
+                    "title": datasets.Value("string"),
+                    "context": datasets.Value("string"),
+                    "question": datasets.Value("string"),
+                    "answers": datasets.features.Sequence(
+                        {
+                            "text": datasets.Value("string"),
+                            "answer_start": datasets.Value("int32"),
+                        }
+                    ),
+                    # These are the features of your dataset like images, labels ...
+                }
+            ),
+            # If there's a common (input, target) tuple from the features,
+            # specify them here. They'll be used if as_supervised=True in
+            # builder.as_dataset.
+            supervised_keys=None,
+            # Homepage of the dataset for documentation
+            homepage="https://rajpurkar.github.io/SQuAD-explorer/",
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+        # TODO(squad_v2): Downloads the data and defines the splits
+        # dl_manager is a datasets.download.DownloadManager that can be used to
+        # download and extract URLs
+        urls_to_download = {"train": os.path.join(_URL, _TRAINING_FILE), "dev": os.path.join(_URL, _DEV_FILE)}
+        downloaded_files = dl_manager.download_and_extract(urls_to_download)
+
+        return [
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": downloaded_files["train"]}),
+            datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={"filepath": downloaded_files["dev"]}),
+        ]
+
+    def _generate_examples(self, filepath):
+        """Yields examples."""
+        # TODO(squad_v2): Yields (key, example) tuples from the dataset
+        with open(filepath, encoding="utf-8") as f:
+            squad = json.load(f)
+            for example in squad["data"]:
+                title = example.get("title", "").strip()
+                for paragraph in example["paragraphs"]:
+                    context = paragraph["context"].strip()
+                    for qa in paragraph["qas"]:
+                        question = qa["question"].strip()
+                        id_ = qa["id"]
+
+                        answer_starts = [answer["answer_start"] for answer in qa["answers"]]
+                        answers = [answer["text"].strip() for answer in qa["answers"]]
+
+                        # Features currently used are "context", "question", and "answers".
+                        # Others are extracted here for the ease of future expansions.
+                        yield id_, {
+                            "title": title,
+                            "context": context,
+                            "question": question,
+                            "id": id_,
+                            "answers": {
+                                "answer_start": answer_starts,
+                                "text": answers,
+                            },
+                        }
